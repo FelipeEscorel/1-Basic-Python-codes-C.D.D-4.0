c9bdff9dd4686423ab5b19573baccbf8e220db7c
Felipe Escorel <felipeescorel@hotmail.com>
2024-09-11 10:01:13 -0300

    Update exercicio016.py

--- exercicio016.py
+++ exercicio016.py
@@ -1,11 +1,11 @@
 i = 0
-nota2 = 0
+soma = 0
 
 alunos = int(input("Digite quantos alunos tem na turma: "))
 
 while i < alunos:
     nota = float(input("Digite um valor: "))
-    nota2 = nota2 + nota
+    soma = soma + nota
     i += 1
-media = nota2/alunos
+media = soma/alunos
 print(f"A média dos alunos é {media}")
